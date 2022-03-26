0c636793518de258b5f59199d05935f7d6093dc7
renangton <puccho0427@gmail.com>
2022-03-27 06:10:39 +0900

    second commit

---
+++ sql/create_pokemon_table_and_insert_data.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS pokemon;
+
+CREATE TABLE pokemon
+(
+	id int unsigned AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL,
+	typePokemon VARCHAR(20) NOT NULL,
+	valuePokemon int(10) NOT NULL,
+
+	PRIMARY KEY(id)
+);
+
+INSERT INTO pokemon (name, typePokemon, valuePokemon) VALUES ("pikatyu", "Electric", 320);
+INSERT INTO pokemon (name, typePokemon, valuePokemon) VALUES ("Bulbasaur", "Grass", 318);
+INSERT INTO pokemon (name, typePokemon, valuePokemon) VALUES ("Charmander", "Fire", 309);
+INSERT INTO pokemon (name, typePokemon, valuePokemon) VALUES ("Squirtle", "Water", 314);
